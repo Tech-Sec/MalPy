63ce8521e0d1c9444fa073253db6768725f5ff09
Imad <65905842+IMADluvsPYTHON@users.noreply.github.com>
2020-06-27 14:12:49 +0300

    Create Webbrowser.py

---
+++ Webbrowser.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+
+while True:
+    webbrowser.open("Enter Any URL")
+    time.sleep(2)
+    
+#This wil make an infinite loop and will start opening tabs of the selected URL with no end :)
+#Just restart your Computert stop this 
