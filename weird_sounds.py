e8a9a040ead465d754f23989c18c075d7eddecbe
Imad <65905842+IMADluvsPYTHON@users.noreply.github.com>
2020-06-27 14:15:02 +0300

    Create weird_sounds.py

---
+++ weird_sounds.py
@@ -0,0 +1,8 @@
+import winsound
+from random import randrange
+
+frequency = randrange(5000)
+duration = randrange(2000)
+winsound.Beep(frequency, duration)
+
+#This code wil make annoying noises on your computer :)
