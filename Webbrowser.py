9f33b36f298ae55b8c79ba9425d5687677ef70ac
Imad <65905842+Tech-Sec@users.noreply.github.com>
2020-09-15 19:22:12 +0300

    Update Webbrowser.py

--- Webbrowser.py
+++ Webbrowser.py
@@ -2,8 +2,9 @@ import webbrowser
 import time
 
 while True:
-    webbrowser.open("Enter Any URL")
+    #Enter Any URL of your choice
+    webbrowser.open("https://google.com")
     time.sleep(2)
     
 #This wil make an infinite loop and will start opening tabs of the selected URL with no end :)
-#Just restart your Computert stop this 
+#Just restart your Computer stop this 
